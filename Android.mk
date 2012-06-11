3b5bc0ea92e3182b3bbc5a85165e1da66ba3e460
Marcin Chojnacki <marcinch7@gmail.com>
2012-06-11 20:08:25 +0200

    first commit

---
+++ Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.gio
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := cameraHAL.cpp
+LOCAL_C_INCLUDES := $(TOP)/frameworks/base/include
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
+LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+LOCAL_SHARED_LIBRARIES += libcamera
+LOCAL_PRELINK_MODULE := false
+
+ifeq ($(BOARD_HAVE_HTC_FFC), true)
+    LOCAL_CFLAGS += -DHTC_FFC
+endif
+ifeq ($(BOARD_USE_REVERSE_FFC), true)
+    LOCAL_CFLAGS += -DREVERSE_FFC
+endif
+
+include $(BUILD_SHARED_LIBRARY)
